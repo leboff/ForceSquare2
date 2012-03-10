63135ac216a42442824ea6b665c25db3682070ba
Bryan Leboff <leboff@gmail.com>
2012-03-10 01:44:26 -0500

    fixed up json pares, i hope

--- app/controllers/foursquare_controller.rb
+++ app/controllers/foursquare_controller.rb
@@ -1,6 +1,7 @@
+
 class FoursquareController < ApplicationController
   def index
-    data = json.parse(param[:checkin])
+    data = JSON.parse(param[:checkin])
     Rails.logger.warn data.to_yaml
   end
 end
