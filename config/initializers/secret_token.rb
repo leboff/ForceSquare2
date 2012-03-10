3772a7bd255d7446ca98676d81881c0f9b46adb5
Bryan Leboff <leboff@gmail.com>
2012-03-09 21:53:52 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ForceSquare2::Application.config.secret_token = 'e62d6b3ce3470d2e09ff590a26778139e0c293572b27eb9d0a2562c4dd70d580319c749ba87bc8a8707198b1d4f31037cb0243800178eaaf27fdaf8ebba8a5ca'
