d7a80b9ab22af596cf329d6ce35e38af97270639
Bryan Leboff <leboff@gmail.com>
2012-03-10 15:59:06 -0500

    checks for the user, and foursquare secret before posting

--- app/classes/poster.rb
+++ app/classes/poster.rb
@@ -6,26 +6,31 @@ class Poster
 
   def initialize(data)
     mdata = Hashie::Mash.new(data)
-    @id = mdata.user.id
+    @user = User.find_by_foursquare_id(mdata.user.id)
     @venue_name = mdata.venue.name
     @checkin_id = mdata.id
+    @secret = mdata.secret
     @bitly = Bitly.new('leboff', 'R_a08016d3520db7a24658de32f04dc51b')
   end
+
+
   def post_to_salesforce
-    the_user.salesforce_orgs.each do |org|
-        headers =  {'Content-Type' => 'application/x-www-form-urlencoded', 'Authorization' => 'OAuth ' + access_token(org.instance, org.token) }
-        options ={ :body =>{ :text => "I just checked in to #{@venue_name} on Forcesquare! " + url, },
-                :headers =>  headers }
-        resp = self.class.post(org.instance+'/services/data/v23.0/chatter/feeds/to/me/feed-items', options)
+    ##TODO: error checks
+    if @user &&  @secret == FOURSQUARE_PUSH_SECRET
+      @user.salesforce_orgs.each do |org|
+          headers =  {'Content-Type' => 'application/x-www-form-urlencoded', 'Authorization' => 'OAuth ' + access_token(org.instance, org.token) }
+          options ={ :body =>{ :text => "I just checked in to #{@venue_name} on Forcesquare! " + url, },
+                  :headers =>  headers }
+          resp = self.class.post(org.instance+'/services/data/v23.0/chatter/feeds/to/me/feed-items', options)
+      end
     end
   end
-  def the_user
-    User.find_by_foursquare_id(@id)
-  end
+
   def url
      @bitly.shorten(the_user.foursquare_url + '/checkin/' +@checkin_id ).short_url
   end
   def access_token(instance, refresh)
+    ##TODO: error checks
     headers =  {'Content-Type' => 'application/x-www-form-urlencoded'}
     options = {:headers=>headers, :body =>{ :grant_type => 'refresh_token', :client_id => SALESFORCE_ID, :client_sercret => SALESFORCE_SECRET, :refresh_token => refresh}}
     resp = self.class.post(instance + '/services/oauth2/token', options)
