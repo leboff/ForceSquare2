b93a0b25f6ee8763f618b4ff7f6d7a6880fffc84
Bryan Leboff <leboff@gmail.com>
2012-03-09 22:11:32 -0500

    starting devise setup, hope this isnt overkill

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,8 @@ ForceSquare2::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  #devise mailer
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
 end
